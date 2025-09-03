50364f1d0d54004a9fc9255d371d02b2e851b29a
orokidd <gregomily@gmail.com>
2025-09-03 17:26:14 +0800

    change preview contact icons

--- src/components/resume/PreviewContact.jsx
+++ src/components/resume/PreviewContact.jsx
@@ -1,20 +1,20 @@
-import { InboxIcon, PhoneIcon, GlobeIcon, HouseIcon } from "../Icons";
+import {InboxIcon2, PhoneIcon2, GlobeIcon2, LocationPin } from "../Icons";
 
 export function Contact({ contact }) {
   return (
     <section className="preview-contacts">
       <h2 className="section-title">Contact</h2>
         <p>
-          <InboxIcon size={22} /> {contact.email || "Your Email"}
+          <InboxIcon2 size={24} /> {contact.email || "Your Email"}
         </p>
         <p>
-          <PhoneIcon size={22} /> {contact.phone || "Your Phone"}
+          <PhoneIcon2 size={24}/> {contact.phone || "Your Phone"}
         </p>
         <p>
-          <GlobeIcon size={22} /> {contact.website || "Your Website"}
+          <GlobeIcon2 size={24} /> {contact.website || "Your Website"}
         </p>
         <p>
-          <HouseIcon size={22} /> {contact.address || "Your Address"}
+          <LocationPin size={24} /> {contact.address || "Your Address"}
         </p>
     </section>
   );
