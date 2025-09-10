c3150843e40101b679737c71bcdd0ed29102ef8b
orokidd <gregomily@gmail.com>
2025-09-10 12:09:16 +0800

    css fix for header mobile view, css fix for form section mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { ResetIcon, EditIcon, DocumentIcon } from "./Icons";
+import { ResetIcon, EditIcon, DocumentIcon, DownloadIcon } from "./Icons";
 
 export default function Header({ isMobile, mode, setMode, onDownload, resetData }) {
   function changeMode() {
@@ -8,11 +8,11 @@ export default function Header({ isMobile, mode, setMode, onDownload, resetData 
   return (
     <header className="header">
       <div className="header-content">
-        <h1>CV Builder</h1>
+        <h1 className="header-title">CV Builder</h1>
         <div className="header-actions">
           <button className="preview-button" onClick={changeMode}>{isMobile && mode === "edit" ? <DocumentIcon size={20} /> : <EditIcon size={20} />}</button>
           <button className="reset-button" onClick={resetData}><ResetIcon size={20} color="rgba(0, 0, 0, 1)" /></button>
-          <button className="download-button" onClick={onDownload}>Download PDF</button>
+          <button className="download-button" onClick={onDownload}><DownloadIcon size={20} /></button>
         </div>
       </div>
     </header>
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -250,3 +250,22 @@ export const DocumentIcon = ({ size }) => (
     <path d="M16 17H8" />
   </svg>
 );
+
+export const DownloadIcon = ({ size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="lucide lucide-download-icon lucide-download"
+  >
+    <path d="M12 15V3" />
+    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+    <path d="m7 10 5 5 5-5" />
+  </svg>
+);
